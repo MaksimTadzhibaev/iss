1efee5e9a6203de894e42140ecb09fa80b8fdd43
miromaxel <miromaxel@yandex.ru>
2023-06-15 12:17:38 +0300

    Выбрал необходимые объекты для возврата

--- src/main/java/ru/tadzh/iss/service/HistoryService.java
+++ src/main/java/ru/tadzh/iss/service/HistoryService.java
@@ -3,6 +3,8 @@ package ru.tadzh.iss.service;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import ru.tadzh.iss.entity.history.ListHistory;
+import ru.tadzh.iss.entity.history.XmlDataHistory;
 import ru.tadzh.iss.entity.history.XmlDocHistory;
 
 import javax.xml.bind.JAXBContext;
@@ -19,14 +21,21 @@ public class HistoryService {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    public XmlDocHistory getHistory() throws JAXBException {
+    public ListHistory getHistory() throws JAXBException {
         String url = "http://iss.moex.com/iss/history/engines/stock/markets/shares/boards/tqbr/securities.xml?date=2013-12-20";
         String body = restTemplate.getForObject(url, String.class);
-        System.out.println(body);
         StringReader reader = new StringReader(body);
         JAXBContext context = JAXBContext.newInstance(XmlDocHistory.class);
         Unmarshaller unmarshaller = context.createUnmarshaller();
         XmlDocHistory doc = (XmlDocHistory) unmarshaller.unmarshal(reader);
-        return doc;
+        XmlDataHistory newData = null;
+        for (XmlDataHistory data : doc.getXmlDataHistory()) {
+            if (data.getListHistory().getHistories().get(0).getTradeDate()!=null){
+                newData = data;
+                break;
+            }
+        }
+        assert newData != null;
+        return newData.getListHistory();
     }
 }
--- src/main/java/ru/tadzh/iss/service/SecuritiesService.java
+++ src/main/java/ru/tadzh/iss/service/SecuritiesService.java
@@ -3,6 +3,7 @@ package ru.tadzh.iss.service;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import ru.tadzh.iss.entity.securities.ListSecurities;
 import ru.tadzh.iss.entity.securities.XmlDocSecurities;
 
 import javax.xml.bind.JAXBContext;
@@ -18,13 +19,13 @@ public class SecuritiesService {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    public XmlDocSecurities getSecurities() throws JAXBException {
+    public ListSecurities getSecurities() throws JAXBException {
         String url = "http://iss.moex.com/iss/securities.xml";
         String body = restTemplate.getForObject(url, String.class);
         StringReader reader = new StringReader(body);
         JAXBContext context = JAXBContext.newInstance(XmlDocSecurities.class);
         Unmarshaller unmarshaller = context.createUnmarshaller();
         XmlDocSecurities doc = (XmlDocSecurities) unmarshaller.unmarshal(reader);
-        return doc;
+        return doc.getXmlDataSecurities().getListSecurities();
     }
 }
